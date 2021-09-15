211cbe2989ec713a315b58dc58ab6d3adfc1d66d
Vasily Chumachenko <v.a.chumachenko@yandex.ru>
2021-09-15 23:33:28 +0300

    5 задание

---
+++ Lesson_2/5.py
@@ -0,0 +1,16 @@
+number = int(input("Введите номер: "))
+my_list = [7, 4, 3, 2]
+c = my_list.count(number)
+for element in my_list:
+    if c > 0:
+        i = my_list.index(number)
+        my_list.insert(i+c, number)
+        break
+    else:
+        if number > element:
+            j = my_list.index(element)
+            my_list.insert(j, number)
+            break
+        elif number < my_list[len(my_list) - 1]:
+            my_list.append(number)
+print(my_list)
